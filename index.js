870d42e25407da146b9c8ba97db04e83d6e941e9
HarishKumar097 <harish.k@fastpix.io>
2024-11-14 06:55:52 +0000

    10837

--- index.js
+++ index.js
@@ -8,14 +8,4 @@ function sub (num1, num2) {
     return num1 - num2
 }
 
-function mul (num1, num2) {
-
-    return num1 * num2
-}
-
-function divide (number1, number2) {
-
-    return number1 / number2
-}
-
-export {add, sub, mul, divide}
+export {add, sub}
